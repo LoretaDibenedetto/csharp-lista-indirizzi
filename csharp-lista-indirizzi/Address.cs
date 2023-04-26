974f65122ef56f7abad7a753a6c37a574a5eef84
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-26 14:41:17 +0200

    Add project files.

---
+++ csharp-lista-indirizzi/Address.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace csharpListaIndirizzi{
+    public class Address
+    {
+       private string name;
+       private string surname;
+       private string street;
+       private string city;
+       private string province;
+       private string ZIP;
+
+
+       public Address(string name, string surname, string street, string city, string province, string ZIP)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;   
+            this.ZIP = ZIP;
+           
+        }
+
+       public string GetName ()
+        {
+            return name;
+        }
+        public string GetSurname() 
+        { 
+            return surname;
+        }
+        public string GetStreet()
+        {
+            return street;
+        }
+        public string GetCity()
+        {
+            return city;
+        }
+        public string GetProvince() 
+        {
+            return province;
+        }
+        public string GetZIP()
+        {
+            return ZIP;
+        }
+
+
+        public override string ToString()
+        {
+           
+           string  strrappresentation = "Nome: " + "  " + this.name + ", ";
+            strrappresentation += "cognome: " + "  " + this.surname + ", ";
+            strrappresentation += "city: " + "   " + this.city + ", ";
+            strrappresentation += "via: " + "  " + this.street + ", ";
+            strrappresentation += "privincia: " + "  " + this.province + ", ";
+            strrappresentation += "zip: " + "  " + this.ZIP + ", ";
+            strrappresentation += "-----------------------------------";
+
+            return strrappresentation;
+
+
+
+        }
+    }
+
+}
---
+++ csharp-lista-indirizzi/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using csharpListaIndirizzi;
+using System;
+using System.Runtime.CompilerServices;
+
+
+//lista di libri vuota
+List<Address> listAddress = new List<Address>();
+//apro file di testo
+StreamReader file = File.OpenText("C:\\Users\\Elle\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\addresses.csv");
+
+//conto le righe partendo da 0 
+int countRow = 0;
+
+//fintanto che il file non e' finito
+while (!file.EndOfStream)
+{
+    //leggi la riga
+    string line = file.ReadLine();
+
+    //aumenta il contatore
+    countRow++;
+
+   // se il contatore delle righe lette e' maggiore di 0
+    if (countRow > 1)
+    {
+            string[] arraySeparatedInfo = line.Split(',');
+        //se l'array length e' diverso da 4 
+        if (arraySeparatedInfo.Length != 6)
+        {
+            //stampa l'errore
+            Console.WriteLine("La riga " + countRow + " non rispetta lo standard richiesto di 6 info per riga");
+            //Address newAddress = new Address(arraySeparatedInfo[0], arraySeparatedInfo[1], arraySeparatedInfo[2], arraySeparatedInfo[3], arraySeparatedInfo[4], arraySeparatedInfo[5]);
+            //listAddress.Add(newAddress);    
+        }
+        else
+        {
+            int zipParse = int.Parse(arraySeparatedInfo[5]);
+            try
+            {
+                Address newAddress = new Address(arraySeparatedInfo[0], arraySeparatedInfo[1], arraySeparatedInfo[2], arraySeparatedInfo[3], arraySeparatedInfo[4], arraySeparatedInfo[5]);
+                listAddress.Add(newAddress);    
+            }catch(ArgumentException e)
+            {
+                Console.WriteLine("l'errore e': " + e.Message);
+            }
+        }
+      }
+
+   // Console.WriteLine(line);
+    
+}
+
+foreach(Address totaddres in listAddress)
+{
+    Console.WriteLine(totaddres);
+}
